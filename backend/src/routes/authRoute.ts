be74a6a17aa2d3c069e263169940ad05dc0102ed
rushikesh <153095920+Shailesh2302@users.noreply.github.com>
2025-11-05 20:32:09 +0530

    auth route added

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -4,20 +4,25 @@ import { prisma } from "../config/prisma";
 import { generateToken } from "../utils/jwt";
 
 export const login = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const user = await prisma.user.findUnique({ where: { email } });
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(401).json({ message: "Invalid credentials" });
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = generateToken({ userId: user.id, role: user.role });
+
+    return res
+      .cookie("jwt", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      })
+      .json({ message: "Login successful", role: user.role });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error: ", error });
   }
-
-  const token = generateToken({ userId: user.id, role: user.role });
-
-  res
-    .cookie("jwt", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-    })
-    .json({ message: "Login successful", role: user.role });
 };
